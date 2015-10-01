7edbd3599857dbc9a72a3106f33545e85e48ecbe
DJ Nadgar <dj@exciteresearch.com>
2015-10-01 19:30:51 -0400

    remastered

---
+++ LinkArchive/secret/development.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "TWITTER": {
+        "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
+        "consumerSecret": "INSERT_TWITTER_CONSUMER_SECRET_HERE",
+        "callbackUrl": "INSERT_TWITTER_CALLBACK_HERE"
+    },
+    "FACEBOOK": {
+        "clientID": "INSERT_FACEBOOK_CLIENTID_HERE",
+        "clientSecret": "INSERT_FACEBOOK_CLIENT_SECRET_HERE",
+        "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
+    },
+    "GOOGLE": {
+        "clientID": "INSERT_GOOGLE_CLIENTID_HERE",
+        "clientSecret": "INSERT_GOOGLE_CLIENT_SECRET_HERE",
+        "callbackURL": "INSERT_GOOGLE_CALLBACK_HERE"
+    },
+    "GITHUB": {
+        "clientID": "1cb7b8d7bce9299eec38",
+        "clientSecret": "8df36b23b717ed6c51493423a8a6998031096b0a",
+        "callbackURL": "http://localhost/callback"
+    },
+    "SLACK": {
+        "clientID": "11489159555.11678878662",
+        "clientSecret": "aadc44518422d05b31e91ffb73ddb71a",
+        "callbackURL": "http://localhost/callback"
+    }
+};
---
+++ LinkArchive/www/js/controllers.js
@@ -0,0 +1,67 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function ($scope, $http, $cordovaOauth) {
+  // testing oauth
+  $scope.slackResponse = 'no request made yet';
+  $scope.slackError = 'no errors yet';
+  $scope.authorize = function () {
+
+    // REMEMBER TO REMOVE THE CLIENT ID AND SECRETS!!!
+    // ***********************************************
+
+    // $cordovaOauth.slack('','',[])
+      // $cordovaOauth.github('GITHUB.clientID','GITHUB.clientSecret',[])
+
+      $cordovaOauth.slack('SLACK.clientID', 'SLACK.clientSecret', ['read'], null)
+    .then(function (result) {
+        $scope.slackResponse = JSON.stringify(result);
+    },
+    function (error) {
+        $scope.slackError = error;
+    });
+  };
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function ($scope) {
+  $scope.values = [ { greeting:'Hello!',
+                    appearance: 'slides blue' },
+                    { greeting: 'Bonjour!',
+                    appearance: 'slides red' },
+                    { greeting: 'Hi!',
+                    appearance: 'slides yellow' } ];
+
+  $scope.cardDestroyed = function(index) {
+    // $scope.values.splice(index, 1);
+  };
+
+  $scope.cardSwiped = function(index) {
+    // var newCard = { greeting: 'Ohayo!', appearance: 'slides purple' };// new card data
+    // $scope.values.push(newCard);
+  };
+  $scope.onSwipeRight = function (index) {
+    // $scope.values.splice(index, 1);
+    // $ionicSlideBoxDelegate.update();
+    // $ionicSlideBoxDelegate.slide(index);
+    // $scope.values.push({ greeting: 'Swipe right works!', 
+    //                      appearance: 'slides purple' });
+  };
+});
