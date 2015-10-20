a10dc9a8b18699a52324f29e0dbeedc6062d5d8b
DJ Nadgar <dj@exciteresearch.com>
2015-10-20 14:11:08 -0400

    get gulp working attempt

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
+// Dependancies
+// Include Gulp Core
 var gulp = require('gulp');
+// Include other plugins
 var gutil = require('gulp-util');
 var bower = require('bower');
 var concat = require('gulp-concat');
@@ -6,13 +9,73 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+// var jshint = require('gulp-jshint');
+// var uglify= require('gulp-uglify');
+
+// Build Dependencies
+var browserify = require('gulp-browserify');
+// Uglify only when ready to publish otherwise it's difficult to debug
+// var uglify = require('gulp-uglify');
+
+// Style Dependencies
+var less = require('gulp-less');
+var prefix = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+
+// Development Dependencies
+var jshint = require('gulp-jshint');
+
+// Test Dependencies
+var mochaPhantomjs = require('gulp-mocha-phantomjs');
+
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss'],
+  lintwww: ['./www/**/*.js'],
+  linttest: ['./test/**/*.js']
 };
 
+// GULP lint wwww 
+gulp.task('lint-www', function() {
+  return gulp.src(paths.lintwww)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('lint-test', function() {
+  return gulp.src(paths.linttest)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('browserify-client', ['lint-www'], function() {
+  return gulp.src('www/js/*.js')
+    .pipe(browserify({
+      insertGlobals: true
+    }))
+    .pipe(rename('bundle.js'))
+//    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('./public/javascripts'));
+});
+
+gulp.task('browserify-test', ['lint-test'], function() {
+  return gulp.src('test/client/index.js')
+    .pipe(browserify({
+      insertGlobals: true
+    }))
+    .pipe(rename('client-test.js'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('www/**/*.js', ['browserify-client']);
+  gulp.watch('test/**/*.js', ['browserify-test']);
+});
+
+// IONIC Gulp tasks below
 gulp.task('default', ['sass']);
 
+// Gulp sass
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass({
@@ -27,10 +90,12 @@ gulp.task('sass', function(done) {
     .on('end', done);
 });
 
+// Gulp watch sass
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
 });
 
+// Gulp install git-check using bower.commands.install()
 gulp.task('install', ['git-check'], function() {
   return bower.commands.install()
     .on('log', function(data) {
@@ -38,6 +103,7 @@ gulp.task('install', ['git-check'], function() {
     });
 });
 
+// Gulp define task git-check
 gulp.task('git-check', function(done) {
   if (!sh.which('git')) {
     console.log(
